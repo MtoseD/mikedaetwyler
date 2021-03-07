1c6cbb96fedd0aa6488ec2361c6d4353ec0bdb39
MtoseD <74704796+MtoseD@users.noreply.github.com>
2021-03-07 02:02:53 +0100

    php / favicon

---
+++ index.php
@@ -0,0 +1,53 @@
+
+<?php include"sites/heading.php";?>
+
+
+    <!-- Flip-Card -->
+
+    <div class="flip-container">
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+                    <img class="flip-card-img" src="images/mikedaetwyler.jpg" alt="Portrait von Mike Dätwyler">
+                </div>
+                <div class="flip-card-back">
+                    <h1>Mike Dätwyler</h1>
+                    <p>Architect & Engineer</p>
+                    <p>We love that guy</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Social Media Buttons -->
+
+    <div class="buttons">
+        <a class="btn" href="https://www.linkedin.com/in/mike-daetwyler" target="_blanket">
+            <i class="fab fa-linkedin"></i>
+        </a>
+        <a class="btn" href="https://www.xing.com/profile/Mike_Daetwyler/cv" target="_blanket">
+            <i class="fab fa-xing-square"></i>
+        </a>
+        <a class="btn" href="https://github.com/MtoseD" target="_blanket">
+            <i class="fab fa-github"></i>
+        </a>
+        <a class="btn" href="mailto:mike.daetwyler@bluewin.ch" target="_blanket">
+            <i class="far fa-envelope"></i>
+        </a>
+    </div>
+
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+    <!-- Footer -->
+
+    <footer>
+
+        <p>Mike Dätwyler</p>
+        <p>Mike Dätwyler</p>
+        <p>Mike Dätwyler</p>
+
+    </footer>
+
+</body>
+
+</html>
---
+++ sites/heading.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="de" class="no-js">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <meta name="description" content="A sidebar menu as seen on the Google Nexus 7 website" />
+    <meta name="keywords" content="google nexus 7 menu, css transitions, sidebar, side menu, slide out menu" />
+    <meta name="author" content="Codrops" />
+    <link rel="stylesheet" type="text/css" href="../css/component.css" />
+    <script src="../js/modernizr.custom.js"></script>
+    <script src="../js/classie.js"></script>
+    <script src="../js/gnmenu.js"></script>
+    <script src="https://kit.fontawesome.com/56a7121549.js" crossorigin="anonymous"></script>
+    <link rel="apple-touch-icon" sizes="57x57" href="../images/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="../images/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="../images/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="../images/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="../images/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="../images/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="../images/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="../images/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="../images/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="../images/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="../images/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../images/favicon/favicon-16x16.png">
+    <link rel="manifest" href="../images/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="../images/favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+</head>
+
+
+
+<body>
+
+    <!-- Navbar -->
+
+    <div class="container">
+        <ul id="gn-menu" class="gn-menu-main">
+            <li class="gn-trigger">
+                <a class="gn-icon gn-icon-menu"><span>Menu</span></a>
+                <nav class="gn-menu-wrapper">
+                    <div class="gn-scroller">
+                        <ul class="gn-menu">
+                            <li>
+                                <input type="checkbox" id="list-item-1">
+                                <label id="list-item-1" for="list-item-1" class="first" class="gn-icon gn-icon-cog">Themen</label>
+                                <ul class="submenu">
+                                    <li class="submenu-li"><a href="">Fake News</a></li>
+                                    <li class="submenu-li"><a href="">Fake News</a></li>
+                                    <li class="submenu-li"><a href="">Fake News</a></li>
+                                    <li class="submenu-li"><a href="">Fake News</a></li>
+                                    <li class="submenu-li"><a href="">Fake News</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <input type="checkbox" id="list-item-2">
+                                <label for="list-item-2" class="first" class="gn-icon gn-icon-cog">Anleitungen</label>
+                                <ul class="submenu">
+                                    <li class="submenu-li"><a href="">Slabo</a></li>
+                                    <li class="submenu-li"><a href="">Slabo</a></li>
+                                    <li class="submenu-li"><a href="">Slabo</a></li>
+                                    <li class="submenu-li"><a href="">Slabo</a></li>
+                                    <li class="submenu-li"><a href="">Slabo</a></li>
+                                </ul>
+                            </li>
+                            <li><a class="gn-icon gn-icon-help">Help</a></li>
+                            <li>
+                                <a class="gn-icon gn-icon-archive">Archives</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </li>
+            <li class="title2"><a class="title" href="">Mike Dätwyler</a></li>
+    </div>
+    <script>
+        new gnMenu(document.getElementById('gn-menu'));
+    </script>
