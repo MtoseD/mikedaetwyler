f674779a1a49884625026e5e15a5882bc7b0e6ef
MtoseD <74704796+MtoseD@users.noreply.github.com>
2021-03-13 00:39:34 +0100

    changed flip-card to image, created themes-site

--- index.php
+++ index.php
@@ -1,44 +1,31 @@
-<?php include"sites/heading.php";?>
+<?php include "sites/heading.php"; ?>
 
 
-    <!-- Flip-Card -->
+<div class="portrait-container">
+    <img class="portrait" src="images/mikedaetwyler.jpg" alt="Portrait von Mike Dätwyler">
+</div>
 
-    <div class="flip-container">
-        <div class="flip-card">
-            <div class="flip-card-inner">
-                <div class="flip-card-front">
-                    <img class="flip-card-img" src="images/mikedaetwyler.jpg" alt="Portrait von Mike Dätwyler">
-                </div>
-                <div class="flip-card-back">
-                    <h1>Mike Dätwyler</h1>
-                    <p>Architect & Engineer</p>
-                    <p>We love that guy</p>
-                </div>
-            </div>
-        </div>
-    </div>
 
-    <!-- Social Media Buttons -->
+<!-- Social Media Buttons -->
 
-    <div class="buttons">
-        <a class="btn" href="https://www.linkedin.com/in/mike-daetwyler" target="_blanket">
-            <i class="fab fa-linkedin"></i>
-        </a>
-        <a class="btn" href="https://www.xing.com/profile/Mike_Daetwyler/cv" target="_blanket">
-            <i class="fab fa-xing-square"></i>
-        </a>
-        <a class="btn" href="https://twitter.com/MikeDaetwyler" target="_blanket">
-            <i class="fab fa-twitter"></i>
-        </a>
-        <a class="btn" href="https://github.com/MtoseD" target="_blanket">
-            <i class="fab fa-github"></i>
-        </a>        
-        <a class="btn" href="mailto:mike.daetwyler@bluewin.ch" target="_blanket">
-            <i class="far fa-envelope"></i>
-        </a>
-    </div>
+<div class="buttons">
+    <a class="btn" href="https://www.linkedin.com/in/mike-daetwyler" target="_blanket">
+        <i class="fab fa-linkedin"></i>
+    </a>
+    <a class="btn" href="https://www.xing.com/profile/Mike_Daetwyler/cv" target="_blanket">
+        <i class="fab fa-xing-square"></i>
+    </a>
+    <a class="btn" href="https://twitter.com/MikeDaetwyler" target="_blanket">
+        <i class="fab fa-twitter"></i>
+    </a>
+    <a class="btn" href="https://github.com/MtoseD" target="_blanket">
+        <i class="fab fa-github"></i>
+    </a>
+    <a class="btn" href="mailto:mike.daetwyler@bluewin.ch" target="_blanket">
+        <i class="far fa-envelope"></i>
+    </a>
+</div>
 
-    <br><br><br><br>
+<br><br><br><br>
 
-    <?php include "sites/footer.php"; ?>
-
+<?php include "sites/footer.php"; ?>
