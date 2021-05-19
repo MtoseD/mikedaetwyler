a08e000b79d772fce739c105bc87b57f354dd6fe
MtoseD <74704796+MtoseD@users.noreply.github.com>
2021-05-19 22:52:58 +0200

    started with securitytips site

---
+++ sites/securityTips.php
@@ -0,0 +1,26 @@
+<?php include "heading.php"; ?>
+
+
+<ul>
+  <li>
+    <a href="#">
+      <h2>Title #1</h2>
+      <p>Text Content #1</p>
+    </a>
+  </li>
+  <li>
+    <a href="#">
+      <h2>Title #2</h2>
+      <p>Text Content #2</p>
+    </a>
+  </li>
+  […]
+</ul>
+
+
+
+
+
+
+
+<?php include "footer.php"; ?>
