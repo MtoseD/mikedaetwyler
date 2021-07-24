33c911c96aff20d7f87de15b098f7a6ec6dbbf0e
MtoseD <74704796+MtoseD@users.noreply.github.com>
2021-07-25 01:52:02 +0200

    added vpn

--- sites/themes.php
+++ sites/themes.php
@@ -8,11 +8,12 @@
     <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Suche nach Themen..." title="Type in a name">
 
     <ul id="myUL">
-        <li><a href="#imgResolution">Bildauflösung</a></li>
+        <li><a href="#imgResolution">Bildauflösungen</a></li>
         <li><a href="#imgTechnology">Bildtechnologien</a></li>
         <li><a href="#browser">Browser</a></li>
         <li><a href="#fakeNews">Fake News</a></li>
-        <li><a class="myUL-last" href="#raspiVSarudino">Raspberry Pi vs Arduino</a></li>
+        <li><a href="#raspiVSarudino">Raspberry Pi vs Arduino</a></li>
+        <li><a class="myUL-last" href="#vpn">VPN</a></li>
     </ul>
 </div>
 
@@ -37,10 +38,10 @@
 </script>
 
 
-<!-- Bildauflösung -->
+<!-- Bildauflösungen -->
 <div class="themes-id" id="imgResolution">placeholder</div>
 <div class="themes-container-content">
-    <h1 class="themes-content-title">Bildauflösung</h1>
+    <h1 class="themes-content-title">Bildauflösungen</h1>
     <p>
     <div class="themes-content-table-container">
         <table class="themes-content-table">
@@ -168,13 +169,17 @@
 <div class="themes-id" id="browser">placeholder</div>
 <div class="themes-container-content">
     <h1 class="themes-content-title">Browser</h1>
+    <h3 class="themes-content-title">Definition</h3>
     <p>
         Browser oder auch Webbrowser (engl. to browse = stöbern...) sind spezielle Computerprogramme zur Darstellung von Webseiten
         im World Wide Web sowie allgemein von Dokumenten und Daten. Das Durchstöbern des World Wide Webs, beziehungsweise das
         aufeinanderfolgende Abrufen beliebiger Hyperlinks als Verbindung zwischen Webseiten mit Hilfe solch eines Programms, wird auch
         als Internetsurfen bezeichnet. Neben HTML-Seiten können Webbrowser verschiedene andere Arten von Dokumenten wie zum Beispiel
         Bilder und PDF-Dokumente anzeigen. Webbrowser stellen die Benutzeroberfläche für Webanwendungen dar.
-        <br><br>
+        
+    </p>
+    <h3 class="themes-content-title">Geschichte</h3>
+    <p>
         Tim Berners-Lee, ein Pionier des Hypertextes, entwickelte ab 1989 am CERN bei Genf (Schweiz) den ersten Webbrowser und -editor
         unter dem Namen WorldWideWeb (später Nexus) auf einer NeXT-Workstation. Eingelagerte Grafiken öffneten sich noch nicht automatisch,
         sondern mussten erst angeklickt werden. Im November 1990 beauftragte er Nicola Pellow mit der Entwicklung des minimalistischen
@@ -208,6 +213,40 @@
         <br><br>
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
     </p>
+</div>
+
+<!-- VPN -->
+<div class="themes-id" id="vpn">placeholder</div>
+<div class="themes-container-content">
+    <h1 class="themes-content-title">VPN</h1>
+    <h3 class="themes-content-title">Was ist VPN und wie funktioniert es?</h3>
+    <p>
+        VPN steht für "Virtual Private Network" und beschreibt die Möglichkeit eine geschützte Netzwerkverbindung unter Nutzung
+        öffentlicher Netzwerke aufzubauen. VPNs verschlüsseln Ihren Internetverkehr und verschleiern Ihre Online-Identität.
+        Damit erschweren sie es Dritten, Ihre Spuren im Internet zu verfolgen und Daten zu stehlen. Die Verschlüsselung findet dabei
+        in Echtzeit statt.<br>
+    </p>
+    <h3 class="themes-content-title">Wie funktioniert VPN?</h3>
+    <p>
+        Ein VPN verbirgt Ihre IP-Adresse, indem das Netz sie über einen speziell konfigurierten Remote-Server umleitet, der von einem VPN-Host
+        ausgeführt wird. Wenn Sie also mit einem VPN online surfen, wird der VPN-Server zum Ursprung Ihrer Daten. Dies macht es Ihrem
+        Internetdienstanbieter (englisch: Internet Service Provider oder ISP) oder anderen Dritten unmöglich zu sehen, welche Webseiten Sie besuchen
+        oder welche Informationen Sie im Internet eingeben. Ein VPN funktioniert wie ein Filter, der alle Daten, die Sie senden und empfangen,
+        in „Kauderwelsch“ verwandelt. Selbst wenn jemand diese Daten in die Hände bekäme, wären sie nutzlos.
+    </p>
+    <h3 class="themes-content-title">Zugriff auf regionale Inhalte</h3>
+    <p>
+        Regionale Webinhalte sind nicht immer für den Zugriff von überall aus freigeschaltet.
+        Dienste und Webseiten enthalten manchmal Inhalte, auf die nur aus bestimmten Teilen der Welt zugegriffen werden kann.
+        Standardverbindungen verwenden lokale Server im Land, um Ihren Standort zu bestimmen. Dies bedeutet, dass Sie auf Reisen nicht auf Inhalte
+        zu Hause und von zu Hause nicht auf internationale Inhalte zugreifen können. Mit VPN-Standort-Spoofing können Sie auf einen Server
+        in anderen Ländern wechseln und Ihren Standort effektiv „ändern“. <br>
+        Somit kann man sich Inhalte, welche bspw. auf Netflix nur in den USA erhältlich ist, auch in der Schweiz ansehen.
+    </p>
+    <i class="arrow-icon fas fa-arrow-right"></i>Bekannte VPN-Anbieter sind beispielsweise: NordVPN, CyberGhost VPN, ProtonVPN...
+    <br><br>
+    <b>Quellen:</b> <a href="https://www.kaspersky.de/resource-center/definitions/what-is-a-vpn" target="_blank">kaspersky.de</a>
+
 </div>
 
 
