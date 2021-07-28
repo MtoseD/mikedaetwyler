68de0446b1cd28d8f9661cc2b24e600f83af1023
MtoseD <74704796+MtoseD@users.noreply.github.com>
2021-07-28 13:19:38 +0200

    adjusted design of helpfulLink
    added also new content to documentariesETC

--- sites/documentariesETC.php
+++ sites/documentariesETC.php
@@ -3,6 +3,7 @@
 
 
 <div class="collapsible-container">
+    <!-- Eigene Projekte -->
     <h2 class="documentaries-title-first">Eigene Projekte</h2>
 
     <button class="collapsible collapsible-first"><a id="lamp-server">Raspberry Pi - LAMP Server</a></button>
@@ -41,6 +42,7 @@
         </object>
     </div>
 
+    <!-- Allgemeines -->
     <h2 class="documentaries-title">Allgemeines</h2>
 
     <button class="collapsible">GitHub - Integration</button>
@@ -63,6 +65,16 @@
 
     <h2 class="documentaries-title">Informatiker - Lehre</h2>
 
+    <!-- Informatiker - Lehre -->
+    <button class="collapsible">Docker - Zusammenfassung</button>
+    <div class="content">
+        <object class="collapsible-object" data="../documents/Zusammenfassung-Docker.pdf" type="application/pdf">
+            <div class=" btn-container mail-container">
+                <a href="../documents/Zusammenfassung-Docker.pdf"><button class="download-btn mail">Download<i class="download-icon fas fa-download"></i></button></a>
+            </div>
+        </object>
+    </div>
+
     <button class="collapsible">ÜK 105 - Zusammenfassung</button>
     <div class="content">
         <object class="collapsible-object" data="../documents/ÜK 105 - Zusammenfassung.pdf" type="application/pdf">
@@ -80,19 +92,10 @@
             </div>
         </object>
     </div>
-
-
-
-    <button class="collapsible"></button>
-    <div class="content">
-        <object class="collapsible-object" data="../documents/ÜK 105 - Zusammenfassung.pdf" type="application/pdf">
-            <div class=" btn-container mail-container">
-                <a href="../documents/ÜK 105 - Zusammenfassung.pdf"><button class="download-btn mail">Download<i class="download-icon fas fa-download"></i></button></a>
-            </div>
-        </object>
-    </div>
 </div>
 
+
+<!-- Script -->
 <script>
     var coll = document.getElementsByClassName("collapsible");
     var i;
--- sites/helpfulLinks.php
+++ sites/helpfulLinks.php
@@ -1,41 +1,69 @@
 <?php include "heading.php"; ?>
 
 
+<div class="helpfulLink-site">
+  <div class="helpfulLink-container">
+    <h1 class="helpfulLink-title">Hilfreiche Links</h1>
 
-<div class="helpfulLink-container">
-  <h1 class="helpfulLink-title">Hilfreiche Links</h1>
-
-  <table class="helpfulLink-table">
-    <tr>
-      <td><a href="https://bm.gianbucher.ch" class="helpfulLink" target="_blank"><div class="link-td">Zusammenfassungen: Berufsmaturität - Technik, Architektur & Life Sciences<i class="fas fa-external-link-alt"></i></div></a></td>
-    </tr>
-    <tr>
-      <td> <a href="https://fersch.de" class="helpfulLink" target="_blank"><div class="link-td">Physik Formelsammlung<i class="fas fa-external-link-alt"></i></i></div></a></td>
-    </tr>
-    <tr>
-      <td> <a href="https://www.youtube.com/channel/UCW5YeuERMmlnqo4oq8vwUpg/playlists" class="helpfulLink" target="_blank"><div class="link-td">Tutorials: PHP, Java, Firebase...<i class="fas fa-external-link-alt"></i></div></a></td>
-    </tr>
-    <tr>
-    <td> <a href="https://www.w3schools.com" class="helpfulLink" target="_blank"><div class="link-td">Syntaxe mit Beispielen: HTML, CSS, PHP, SQL, Java...<i class="fas fa-external-link-alt"></i></div></a></td>
-    </tr>
-    <tr>
-    <td> <a href="https://fontawesome.com" class="helpfulLink" target="_blank"><div class="link-td">Icons für deine Website<i class="fas fa-external-link-alt"></i></div></a></td>
-    </tr>
-    <tr>
-    <td> <a href="https://www.favicon-generator.org" class="helpfulLink" target="_blank"><div class="link-td">Favicon für deine Website<i class="fas fa-external-link-alt"></i></div></a></td>
-    </tr>
-    <tr>
-    <td> <a href="https://www.1001freefonts.com/de/" class="helpfulLink" target="_blank"><div class="link-td">Diverse Schriftarten für deine Website<i class="fas fa-external-link-alt"></i></div></a></td>
-    </tr>
-    <tr>
-      <td> <a href="https://www.hostpoint.ch/domains/domains.html" class="helpfulLink" target="_blank"><div class="link-td">Domain kaufen für deine Website<i class="fas fa-external-link-alt"></i></div></a></td>
-    </tr>
-    <tr>
-      <td> <a href="https://getbootstrap.com" class="helpfulLink" target="_blank"><div class="link-td">CSS-Framework<i class="fas fa-external-link-alt"></i></div></a></td>
-    </tr>
-  </table>
+    <table class="helpfulLink-table">
+      <tr>
+        <td><a href="https://bm.gianbucher.ch" class="helpfulLink" target="_blank">
+            <div class="link-td">Zusammenfassungen: Berufsmaturität - Technik, Architektur & Life Sciences<i class="fas fa-external-link-alt"></i></div>
+          </a></td>
+      </tr>
+      <tr>
+        <td> <a href="https://fersch.de" class="helpfulLink" target="_blank">
+            <div class="link-td">Physik Formelsammlung<i class="fas fa-external-link-alt"></i></i></div>
+          </a></td>
+      </tr>
+      <tr>
+        <td> <a href="https://www.youtube.com/channel/UCW5YeuERMmlnqo4oq8vwUpg/playlists" class="helpfulLink" target="_blank">
+            <div class="link-td">Tutorials: PHP, Java, Firebase...<i class="fas fa-external-link-alt"></i></div>
+          </a></td>
+      </tr>
+      <tr>
+        <td> <a href="https://www.w3schools.com" class="helpfulLink" target="_blank">
+            <div class="link-td">Syntaxe mit Beispielen: HTML, CSS, PHP, SQL, Java...<i class="fas fa-external-link-alt"></i></div>
+          </a></td>
+      </tr>
+      <tr>
+        <td> <a href="https://fontawesome.com" class="helpfulLink" target="_blank">
+            <div class="link-td">Icons für Ihre Website<i class="fas fa-external-link-alt"></i></div>
+          </a></td>
+      </tr>
+      <tr>
+        <td> <a href="https://www.favicon-generator.org" class="helpfulLink" target="_blank">
+            <div class="link-td">Favicon für Ihre Website<i class="fas fa-external-link-alt"></i></div>
+          </a></td>
+      </tr>
+      <tr>
+        <td> <a href="https://www.1001freefonts.com/de/" class="helpfulLink" target="_blank">
+            <div class="link-td">Diverse Schriftarten für Ihre Website<i class="fas fa-external-link-alt"></i></div>
+          </a></td>
+      </tr>
+      <tr>
+        <td> <a href="https://www.hostpoint.ch/domains/domains.html" class="helpfulLink" target="_blank">
+            <div class="link-td">Domain kaufen für Ihre Website<i class="fas fa-external-link-alt"></i></div>
+          </a></td>
+      </tr>
+      <tr>
+        <td> <a href="https://htmlcolorcodes.com" class="helpfulLink" target="_blank">
+            <div class="link-td">Color Codes<i class="fas fa-external-link-alt"></i></div>
+          </a></td>
+      </tr>
+      <tr>
+        <td> <a href="https://getbootstrap.com" class="helpfulLink" target="_blank">
+            <div class="link-td">CSS-Framework<i class="fas fa-external-link-alt"></i></div>
+          </a></td>
+      </tr>
+      <tr>
+        <td> <a href="https://search.google.com/search-console/about?hl=de" class="helpfulLink" target="_blank">
+            <div class="link-td">Google Search Console<i class="fas fa-external-link-alt"></i></div>
+          </a></td>
+      </tr>
+    </table>
+  </div>
 </div>
-
 
 
 <?php include "footer.php"; ?>
