29678f06ab705ff9f713921b1769b4c2c0acbdc9
MtoseD <74704796+MtoseD@users.noreply.github.com>
2021-07-30 09:28:53 +0200

    adjusted securityTips

--- sites/securityTips - final.php
+++ sites/securityTips - final.php
@@ -1,8 +1,5 @@
 <?php include "heading.php"; ?>
 
-<!-- Java Script -->
-
-
 
 
 <!-- Post-Its -->
@@ -33,7 +30,7 @@
     </a>
   </li>
   <li class="security-li">
-    <a class="security-a" href="#openModal3">
+    <a class="security-a" href="#openModal4">
       <h2 class="security-h2">Passwörter</h2>
       <p class="security-p">
         <ul class="security-p-ul">
@@ -51,7 +48,7 @@
 
 <div id="openModal1" class="modalDialog">
   <div>
-    <a href="#close" title="Close" class="close"><i class="far fa-times-circle"></i></a>
+    <a href="#close" title="close" class="close"><i class="far fa-times-circle"></i></a>
     <h2>Modal Box 1</h2>
     <p>This is a sample modal box that can be created using the powers of CSS3. </p>
     <p>You could do a lot of things here like have a pop-up ad that shows when your website loads, or create a login/register form for users.</p>
@@ -59,7 +56,7 @@
 </div>
 <div id="openModal2" class="modalDialog">
   <div>
-    <a href="#close" title="Close" class="close"><i class="far fa-times-circle"></i></a>
+    <a href="#close" title="close" class="close"><i class="far fa-times-circle"></i></a>
     <h2>Modal Box 2</h2>
     <p><strong>Box 2</strong></p>
     <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
@@ -67,7 +64,7 @@
 </div>
 <div id="openModal3" class="modalDialog">
   <div>
-    <a href="#close" title="Close" class="close"><i class="far fa-times-circle"></i></a>
+    <a href="#close" title="close" class="close"><i class="far fa-times-circle"></i></a>
     <h2>Modal Box 2</h2>
     <p><strong>Box 2</strong></p>
     <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
@@ -75,7 +72,7 @@
 </div>
 <div id="openModal4" class="modalDialog">
   <div>
-    <a href="#close" title="Close" class="close"><i class="far fa-times-circle"></i></a>
+    <a href="#close" title="close" class="close"><i class="far fa-times-circle"></i></a>
     <h2>Modal Box 1</h2>
     <p>This is a sample modal box that can be created using the powers of CSS3. </p>
     <p>You could do a lot of things here like have a pop-up ad that shows when your website loads, or create a login/register form for users.</p>
@@ -96,47 +93,6 @@
     $(".popup_body").removeClass("popup_body_show");
   });
 </script>
-
-
-
-
-
-
-
-
-<!-- 
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet"/>
-
- Trigger the modal with a button 
-<a href="#Modal" class="btn btn-info btn-lg">Open modal</a>
-Modal 
-<div id="Modal" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-
- Modal content
-<div class="modal-content">
-  <div class="modal-header">
-    <button type="button" class="close" data-dismiss="modal">&times;</button>
-    <h4 class="modal-title">Modal Header</h4>
-  </div>
-  <div class="modal-body">
-    <p>Some text in the modal.</p>
-  </div>
-  <div class="modal-footer">
-    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-  </div>
-</div>
-
-  </div>
-</div>
-<script>
-$('a[href$="#Modal"]').on( "click", function() {
-   $('#Modal').modal('modal-content');
-});
-</script> -->
 
 
 
